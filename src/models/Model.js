bfed6135639b3c84a1fc37ec3f50077d9b1a42f8
Samuel Davis <samueljakdavis@gmail.com>
2019-01-10 02:26:49 -0500

    omit console log

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,6 +1,5 @@
 export default class Model {
     constructor(data = {}) {
-        console.log({props: this.properties, data})
         Object.keys(this.properties).forEach((prop) => this[prop] = data[prop])
     }
 
